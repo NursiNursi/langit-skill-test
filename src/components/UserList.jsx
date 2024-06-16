e6be819c51739930a5eaf4b00ac8d64637e0c717
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2024-06-16 08:18:59 +0700

    Add Delete User Feature

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,11 +1,16 @@
 import { Col, Container, Row, Table } from "react-bootstrap";
 
-import { fetchAllUsers } from "../redux/actions/userActions";
-import { useEffect } from "react";
+import { deleteUser, fetchAllUsers } from "../redux/actions/userActions";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import AddUser from "./AddUser";
+import { AiFillDelete } from "react-icons/ai";
+import ModalComponent from "./ui/ModalComponent";
 
-function UserList({ userData, fetchAllUsers }) {
+function UserList({ userData, fetchAllUsers, deleteUser }) {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
   useEffect(() => {
     const getAllUsers = async () => {
       try {
@@ -23,40 +28,87 @@ function UserList({ userData, fetchAllUsers }) {
     getAllUsers();
   }, [fetchAllUsers]);
 
+  const handleDelete = (userInfo) => {
+    setShowDeleteModal(true);
+    setSelectedUser(userInfo);
+  };
+
+  const confirmDelete = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${selectedUser.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok === true) {
+          deleteUser(selectedUser.id);
+        }
+        setShowDeleteModal(false);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+
   return (
-    <Container>
-      <Row className="py-5 mt-5">
-        <AddUser />
-      </Row>
-      <Row>
-        <Col>
-          <Table hover responsive="sm">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Email</th>
-                <th>Company</th>
-                <th>City</th>
-                <th>Website</th>
-              </tr>
-            </thead>
-            <tbody>
-              {userData?.map((user, index) => (
-                <tr key={user.id}>
-                  <td>{index + 1}</td>
-                  <td className="fw-bold">{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{user.company.name}</td>
-                  <td>{user.address.city}</td>
-                  <td>{user.website}</td>
+    <>
+      <Container>
+        <Row className="py-5 mt-5">
+          <AddUser />
+        </Row>
+        <Row>
+          <Col>
+            <Table hover responsive="sm">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama</th>
+                  <th>Email</th>
+                  <th>Company</th>
+                  <th>City</th>
+                  <th>Website</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-    </Container>
+              </thead>
+              <tbody>
+                {userData?.map((user, index) => (
+                  <tr key={user.id}>
+                    <td>{index + 1}</td>
+                    <td className="fw-bold">{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.company.name}</td>
+                    <td>{user.address.city}</td>
+                    <td>{user.website}</td>
+                    <td>
+                      <Row>
+                        <Col>
+                          <AiFillDelete
+                            onClick={() => handleDelete(user)}
+                            color="red"
+                            role="button"
+                          />
+                        </Col>
+                      </Row>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+      {showDeleteModal && (
+        <ModalComponent
+          showModal={showDeleteModal}
+          setShowModal={setShowDeleteModal}
+          confirmAction={confirmDelete}
+          title="Delete User"
+          content="Are you sure you want to delete this user ?"
+          confirmButtonText="Confirm"
+          cancelButtonText="Cancel"
+        />
+      )}
+    </>
   );
 }
 
@@ -69,6 +121,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchAllUsers: (data) => dispatch(fetchAllUsers(data)),
+    deleteUser: (id) => dispatch(deleteUser(id)),
   };
 };
 
